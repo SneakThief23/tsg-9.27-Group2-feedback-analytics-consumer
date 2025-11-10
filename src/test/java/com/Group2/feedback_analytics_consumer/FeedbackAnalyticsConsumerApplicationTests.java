a21811f59aaabdd85e860bc55cfc7a46a106893a
SneakThief <nyxalis@gmail.com>
2025-11-10 13:02:12 -0500

    Initial commit

---
+++ src/test/java/com/Group2/feedback_analytics_consumer/FeedbackAnalyticsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Group2.feedback_analytics_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackAnalyticsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
