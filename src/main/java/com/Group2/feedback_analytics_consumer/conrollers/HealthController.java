60b68c424530b20985b180da696e8dc0718bce9d
SneakThief <nyxalis@gmail.com>
2025-11-10 13:05:44 -0500

    Added java classes and file structure

---
+++ src/main/java/com/Group2/feedback_analytics_consumer/conrollers/HealthController.java
@@ -0,0 +1,4 @@
+package com.Group2.feedback_analytics_consumer.conrollers;
+
+public class HealthController {
+}
---
+++ src/main/java/com/Group2/feedback_analytics_consumer/messaging/KafkaConsumerConfig.java
@@ -0,0 +1,4 @@
+package com.Group2.feedback_analytics_consumer.messaging;
+
+public class KafkaConsumerConfig {
+}
