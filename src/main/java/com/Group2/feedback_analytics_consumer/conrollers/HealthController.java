1a22ec3c5c7a616ec7826a37f857115451f3fc3d
Abdulkader <aabdulakader@gmail.com>
2025-11-11 23:06:15 -0600

    Added /health controller and test class

--- src/main/java/com/Group2/feedback_analytics_consumer/conrollers/HealthController.java
+++ src/main/java/com/Group2/feedback_analytics_consumer/conrollers/HealthController.java
@@ -1,4 +1,17 @@
 package com.Group2.feedback_analytics_consumer.conrollers;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController("/")
 public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<Map> health() {
+        return  ResponseEntity.ok().body(Map.of("status", "OK"));
+    }
 }
---
+++ src/test/java/com/Group2/feedback_analytics_consumer/conrollers/HealthControllerTest.java
@@ -0,0 +1,13 @@
+package com.Group2.feedback_analytics_consumer.conrollers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class HealthControllerTest {
+
+    @Test
+    void health() {
+    }
+}
